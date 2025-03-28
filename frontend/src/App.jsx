1ccc400fdf438d2fc7068a543d2314c60866fd80
dhanshree2025 <dhanshreekakde0@gmail.com>
2025-03-28 10:20:15 +0530

    created routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Doctors from './Pages/Doctors'
+import Login from './Pages/Login'
+import About from './Pages/About'
+import MyProfile from './Pages/MyProfile'
+import Contact from './Pages/Contact'
+import MyAppointments from './Pages/MyAppointments'
+import Appointment from './Pages/Appointment'
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-appointments' element={<MyAppointments/>}/>
+        <Route path='/appointments' element={<Appointment/>}/>
+        <Route path='/appointments/:docId' element={<Appointment/>}/>
+
+      </Routes>
     </div>
   )
 }
