8f9f30b1dca63e96915a11b7cfb6e3d5ae24b356
dhanshree2025 <dhanshreekakde0@gmail.com>
2025-03-28 23:35:15 +0530

    small change in css

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     <div className='bg-green-600 rounded-lg h-auto mt-6 md:px-10 lg:px-20 flex flex-col  flex-wrap md:flex-row'>
         {/*--------Left side---------*/}
         <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[30px]'>
-             <p className='w-96 text-2xl md:text-3xl lg:text-4xl text-white font-semibold leading-tight md:lead lg:leading-tight'>Book Appointment With Trusted Doctors</p>
-             <div className='flex flex-col md:flex-row gap-4 items-center text-white text-sm font-light'>
+             <p className='w-96 text-2xl md:text-3xl lg:text-4xl text-white font-semibold leading-tight md:lead lg:leading-tight'>Book Appointments With Trusted Doctors</p>
+             <div className='flex flex-col md:flex-row gap-4 items-center text-white text-base font-light'>
                 <img className='w-28' src={assets.group_profiles} alt="" />
                 <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/>schedule your appointment hassle-free.</p>
              </div>
