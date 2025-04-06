50fd5e632f7f4bf56f19d6bc32bb86f34200b6b6
dhanshree2025 <dhanshreekakde0@gmail.com>
2025-04-06 09:57:22 +0530

    added css properties in MyAppointments page

--- frontend/src/Pages/MyAppointments.jsx
+++ frontend/src/Pages/MyAppointments.jsx
@@ -7,25 +7,25 @@ const MyAppointments = () => {
 
   return (
     <div className='mt-32 ml-14 mr-14'>
-        <p>My Appointments</p>
+        <p className='pb-3 font-medium text-zinc-700 border-b'>My Appointments</p>
         <div>
-          {doctors.slice(0,2).map((Item,index)=>(
-            <div key={index}>
+          {doctors.slice(0,3).map((Item,index)=>(
+            <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
                <div>
-                <img src={Item.image} alt="" />
+                <img className='w-32 bg-indigo-50' src={Item.image} alt="" />
                </div>
-               <div>
-                  <p>{Item.name}</p>
+               <div className='flex-1 text-sm text-zinc-600'>
+                  <p className='text-neutral-800 font-semibold'>{Item.name}</p>
                   <p>{Item.speciality}</p>
-                  <p>Address:</p>
-                  <p>{Item.address.line1}</p>
-                  <p>{Item.address.line2}</p>
-                  <p><span>Date & Time:</span>25,July,2024 | 8:30PM</p>
+                  <p className='text-zinc-700 font-medium mt-1'>Address:</p>
+                  <p className='text-xs'>{Item.address.line1}</p>
+                  <p className='text-xs'>{Item.address.line2}</p>
+                  <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span>25,July,2024 | 8:30PM</p>
                </div>
                <div></div>
-               <div>
-                <button>Pay Online</button>
-                <button>Cancle Appointment</button>
+               <div className='flex flex-col gap-2 justify-end'>
+                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-green-400 hover:text-white transition-all duration-300'>Pay Online</button>
+                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancle Appointment</button>
                </div>
             </div>
             
